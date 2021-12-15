1ac48c4e4268abb56dc4d1988f9759589226231e
Chris Woods <chris.woods@gmail.com>
2021-12-14 21:44:09 -0500

    working example of string slices

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
 
     let word = first_word(&s);
 
-    s.clear();
+    println!("{}", word);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
